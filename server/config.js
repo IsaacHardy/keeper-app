fb31b350774e16b2ae0eb5ac5defa336bbf53c28
Isaac Hardy <isaachardyDEV@gmail.com>
2016-08-06 17:56:39 -0400

    Updated MongoDB uri

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/fantasy-football',
+  mongoURL: process.env.MONGO_URL || 'mongodb://admin:iloveGod4ever@ds145245.mlab.com:45245/heroku_q3b2tpgf',
   port: process.env.PORT || 8000,
 };
 
